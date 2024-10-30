5609a1a58c7609d77ad8bff7d0b402992e767195
Nastya <anastasion0107@gmail.com>
2024-10-31 03:14:30 +0700

    создала проект

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('register/', views.register, kwargs={"name":"Сучка", "age":"12"}, name='register'),
+    path('register/', views.register, kwargs={"name":"Паша", "age":"12"}, name='register'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote')
